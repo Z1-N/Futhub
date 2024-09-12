5fee13e8dfc3b6650518d5d5e5e961894e8b24d9
Eltayib <alltayeb112003@gmail.com>
2024-09-12 22:02:03 +0400

    small cors removal

--- src/Components/MainSection.jsx
+++ src/Components/MainSection.jsx
@@ -20,7 +20,7 @@ const leagues = [
   { id: 'FL1', name: 'Ligue 1' },
   { id: 'CL', name: 'Champions League' },
 ];
-const baseURL = 'https://cors-anywhere.herokuapp.com/https://api.football-data.org/v4/competitions';
+const baseURL = 'https://api.football-data.org/v4/competitions';
 
 const MainMatchResult = () => {
   const [leagueMatches, setLeagueMatches] = useState({});
